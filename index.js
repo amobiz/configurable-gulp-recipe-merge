606e8a8beb1572f17d6e9596f18d90598017f121
amobiz <amobiz.tw+github@gmail.com>
2016-01-11 05:16:03 +0800

    delegates to gulp-ccr-stream-helper

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 
-var helper = require('gulp-ccr-helper');
-var verify = helper.verifyStreamPrerequisite('merge');
+var helper = require('gulp-ccr-stream-helper')('merge');
 
 /**
  * Recipe:
@@ -21,14 +20,29 @@ function merge() {
 	// lazy loading required modules.
 	var mergeStream = require('merge-stream');
 
+	var gulp = this.gulp;
+	var config = this.config;
+	var upstream = this.upstream;
 	var tasks = this.tasks;
-	var runTask = verify(this);
+
+	helper.prerequisite(this, true, 1);
 
 	if (tasks.length === 1) {
 		return runTask(tasks[0]);
 	}
 
 	return mergeStream(tasks.map(runTask));
+
+	function runTask(task) {
+		var context;
+
+		context = {
+			gulp: gulp,
+			config: config,
+			upstream: upstream
+		};
+		return helper.runTask(context, task);
+	}
 }
 
 merge.expose = [];
